b26b40ccb93b2fe0b291b524b6fcadeeae37810c
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-18 20:51:09 -0700

    Simplified countdown formula after rewatching content, this makes more sense to me now!

--- lesson02/index.js
+++ lesson02/index.js
@@ -1,8 +1,10 @@
 // countdown.js
 
-let number = 10; // You can change this to start from any number
-
-while (number >= 0) {
-  console.log(number);
-  number--;
+for (let number=0;number < 5; number++) {
+  console.log ('hola');
 }
+
+// countdown.js - this countdown will stop at 5 
+
+// Example from the internet -,I understand this in theory - but not completely. '--' means decrement: 
+// let number = 10; while (number >= 0) { console.log(number); number--;}
