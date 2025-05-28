7b1a22b615870ea869602de79c2fa3b2ab0675b2
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-28 15:44:45 -0700

    Added notes to lesson 2

--- lesson02/index.js
+++ lesson02/index.js
@@ -5,6 +5,7 @@ for (let number=0;number < 5; number++) {
 }
 
 // countdown.js - this countdown will stop at 5 
+// https://codepen.io/umbarlumbar/pen/oggrqvW?editors=1112
 
 // Example from the internet -,I understand this in theory - but not completely. '--' means decrement: 
 // let number = 10; while (number >= 0) { console.log(number); number--;}
