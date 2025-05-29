e3c82045e91ed7c29787b537cf41921b3453f46e
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-28 17:57:27 -0700

    Lesson 4 - Challenge 3 (New JS file)
    Having problems connecting local to remote - adding manually for now.

---
+++ lesson04/challenge2.js
@@ -0,0 +1,11 @@
+// Create a JavaScript file that does the following:
+
+//Take a sentence of at least five words and a period.
+//Make an array from that sentence and console.log the third word.
+
+
+const sentence = "JavaScript is difficult, but I am learning.";
+const words = sentence.split(" ");
+console.log(words[2]); 
+
+// Output: "difficult,"
---
+++ lesson04/challenge3.js
@@ -0,0 +1,14 @@
+const animals = ["dog", "snake", "guineapig", "cat", "rabbit"];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(i + ", " + animals[i]);
+}
+
+//output is
+// "0, dog"
+//"1, snake"
+//"2, guineapig"
+//"3, cat"
+//"4, rabbit"
+// see code pen: https://codepen.io/umbarlumbar/pen/emNNjPQ?editors=1112
+
