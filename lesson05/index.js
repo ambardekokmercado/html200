5b68ed54662758ba0fe76ca4959c4e46a3bf6a6b
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-29 19:10:25 -0700

    lesson-5-challenge1
    Adding index and JS files

---
+++ lesson05/index.js
@@ -0,0 +1,30 @@
+//Write a simple webpage that contains a button. 
+ //When users click the button it should open a prompt that takes a number 
+ //from the user and squares it. Next, it should open a second alert with the result. 
+ //Create an HTML page and write your JavaScript in a script tag, or create a JavaScript 
+ //file and link to it from the HTML page.
+
+//You must use a function and an event handler.
+
+
+//Declare variables (?)
+//Name function
+//Result 
+
+
+
+function showPrompt() {
+ let cat= prompt ("Enter a number to square!");
+ let dog= cat*cat;
+ alert (dog);
+}
+
+
+
+
+
+
+//function numberSquared (firstNumber,firstNumber)
+//result = firstNumber*firstNumber
+
+//numberSquared (x,x)
