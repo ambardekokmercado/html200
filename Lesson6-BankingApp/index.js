29bf5dbd97b3d9bbcf135186a21c0998ee9fb16d
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-30 22:22:13 -0700

    Lesson 6 - BankingApp

---
+++ Lesson6-BankingApp/index.js
@@ -0,0 +1,38 @@
+ 
+
+function quitMessage(message) { 
+alert (message);
+//This opens an alert message that notifies the user they are quitting the application
+} 
+
+function withdrawMessage(message) { 
+let catMoney = prompt (message);
+// alert (catMoney);
+// The prompt asks users to enter withdrawal amount.
+// catMoney is the variable storing the input information - which we then can show as an alert back to the user. I used the alert to test. 
+} 
+
+
+function depositMessage(message) { 
+let dogMoney = prompt (message);
+//alert (dogMoney);
+//The prompt asks users to enter deposit amount.
+// dogMoney is the variable storing the input information - which we then can show as an alert back to the user. I used the alert to test. 
+} 
+
+
+function balanceMessage(message){
+alert (message);
+}
+
+
+//let startingBalance =1000;
+//let updatedBalance = startingBalance+dogMoney-catMoney;
+//function balanceMessage(message){
+//alert (message)+(updatedBalance);
+//}
+
+
+
+
+
